b35d66bc7a2a4a81390b1117264af79c2dab278b
Diogo Fouto <diogo.fouto@protonmail.com>
2021-10-20 17:43:35 +0100

    Implemented Prisoner's Dilemma

--- player.py
+++ player.py
@@ -1,47 +1,89 @@
 import networkx as nx
+import numpy as np
 from .arena import Arena
 
 
 class Player:
-    # Network where all players reside
-    network = None
 
-    # Average bias between tags
-    global_bias = [[0.5, 0.5], [0.5, 0.5]]
+    # ---------------------- STATIC VARIABLES -----------------------
 
-    def __init__(self, arena=None, player_id=None, player_info=None, tag=0.5):
-        # Required argument
+    network = None # where all players reside
+    global_bias = [[0.5, 0.5], [0.5, 0.5]] # bias between tags
+
+
+    # ----------------------- MAIN FUNCTIONS ------------------------
+
+    def __init__(self, arena=None, player_id=None):
+        # Check arguments
         assert arena is not None, TypeError('No arena given.')
+        assert player_id is not None, TypeError('No player_id given.')
 
-        # Newborn player parameters
+        # Newborn player params
         self.id = player_id
-        self.info = player_info
-        self.tag = tag
+        self.tag = 0.5
         self.bias = Player.global_bias
+        self.payoff = 0
 
         # Initialize network
         Player.network = arena.G
 
         # For SimPy
         self.env = arena
-        self.action = self.env.process(self.run())
+        self.env.process(self.run())
 
     def run(self):
         while True:
-            self.play()
+            self.play_with_neighbors()
 
-    # Prisoner's Dilemma
-    def play(self):
-        self.interact_with_all_neighbors()
-        self.update_strategy()
 
-    def interact_with_all_neighbors(self):
-        for player in self.get_neighboring_players():
-            # beats me
+    # --------------------- PRISONER'S DILEMMA ----------------------
+    
+    def play_with_neighbors(self):
+        for player in self.get_neighbors():
+            # Choose own's tactic
+            own_cooperate_prob = self.bias[round(self.tag)][round(player.tag)]
+            own_choice = np.random.choice(a=['C', 'D'], p=[own_cooperate_prob, 1-own_cooperate_prob])
+            
+            # Get other player's tactic
+            other_cooperate_prob = player.bias[round(player.tag)][round(self.tag)]
+            other_choice = np.random.choice(a=['C', 'D'], p=[other_cooperate_prob, 1-other_cooperate_prob])
 
-    def update_strategy(self):
+            # Play
+            own_previous_payoff = self.payoff
+            other_previous_payoff = player.payoff
+
+            if own_choice == other_choice:
+                if own_choice == 'C':
+                    self.payoff += 1
+                    player.payoff += 1
+                else:
+                    self.payoff += 2
+                    player.payoff += 2
+            else:
+                if own_choice == 'D':
+                    player.payoff += 3
+                else:
+                    self.payoff += 3
+
+            # Update own bias
+            if self.payoff > own_previous_payoff:
+                # TODO
+            else:
+                # TODO
+
+            # Update other player's bias
+            if player.payoff > other_previous_payoff:
+                # TODO
+            else:
+                # TODO
+
+
+    def update_bias(self, game_score=None, other_player_tag=None):
         raise NotImplementedError(self)
 
+
+    # ---------------------- HELPER FUNCTIONS -----------------------
+    
     def get_all_nodes(self):
         return Player.network.nodes()
 
@@ -52,7 +94,7 @@ class Player:
             players = self.get_all_nodes()
 
         if tag_id is None:
-            return [Player.network.node[_]['player'] for _ in players]  # return all regardless of tag
+            return [Player.network.node[_]['player'] for _ in players]
         else:
             return [Player.network.node[_]['player'] for _ in players
                     if Player.network.node[_]['player'].tag['id'] == tag_id]
@@ -60,33 +102,8 @@ class Player:
     def get_all_players(self, tag_id=None):
         return self.get_players(tag_id=tag_id, limit_neighbors=False)
 
-    def get_neighboring_players(self, tag_id=None):
+    def get_neighbors(self, tag_id=None):
         return self.get_players(tag_id=tag_id, limit_neighbors=True)
-
-    def get_neighboring_nodes(self):
-        return Player.network.neighbors(self.id)
 
     def get_player(self, player_id):
         return Player.network.node[player_id]['player']
-
-    def add_node(self, player_type=None, tag=None):
-        player_id = int(len(Player.network.nodes()))
-        player = player_type(self.env, player_id=player_id, tag=tag)
-        Player.network.add_node(player_id, {'player': player})
-        return player_id
-
-
-    def add_edge(self, player_id1, player_id2):
-        if player_id1 in Player.network.nodes(data=False):
-            if player_id2 in Player.network.nodes(data=False):
-                Player.network.add_edge(player_id1, player_id2)
-            else:
-                raise ValueError('\'Player 2\'[{}] doesn\'exist'.format(player_id2))
-        else:
-            raise ValueError('\'Player 1\'[{}] doesn\'exist'.format(player_id1))
-
-    def remove_node(self, player_id):
-        Player.network.remove_node(player_id)
-
-    def die(self):
-        self.remove_node(self.id)
