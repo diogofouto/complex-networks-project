24eaf19fde5db8b59ea76c84ff921fbeaec41cc3
JFonseca25 <joao.b.fonseca@tecnico.ulisboa.pt>
2021-10-26 14:14:03 +0100

    play and belief_update are done

--- arena.py
+++ arena.py
@@ -15,16 +15,22 @@ class Arena(simpy.Environment):
     Returns a list of the opinions and a list of global biases for each timestep
     """
     def run(self, num_timesteps):
-        opinions = []# A list of dictionaries for each timestep, with opinions for each id
-        global_biases = []    # A list of dictionaries for each timestep, with prejudice for each pair
+        opinions = []       # A list of dictionaries for each timestep, with opinions for each id
+        global_biases = []  # A list of lists for each timestep, with prejudice for each pair
                             # That is, the table P for AA, AB, BA and BB
 
-
+        # Running the simulation for num_timesteps
         for i in range(0, num_timesteps):
-            super.run(1) #? wot, lad
-
+            # Play the game
             for u, v in [edge for edge in self.G.edges]:
-                self.G[u]['player'].run(self.G[v]['player'])
+                self.G[u]['player'].play_with_neighbour(self.G[v]['player'])
+
+            # Update the individual biases after each round
+            for u in self.G.nodes:
+                self.G[u]['player'].update_bias()
+
+            # TODO: Update global biases with the average of each tag's beliefs.
+            # TODO: Resets counters.
 
             #Store statistics: current global bias and all players opinion
             global_biases += Player.global_bias
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+import math
 from .arena import Arena
 
 
@@ -8,8 +9,8 @@ class Player:
     # ---------------------- STATIC VARIABLES -----------------------
 
     network = None # where all players reside
-    global_bias = [[0.5, 0.5], [0.5, 0.5]] # bias between tags
-
+    global_bias = [[0.5, 0.5], [0.5, 0.5]] # bias towards thinking that a player is cooperator / defactor
+    FORGETTING_FACTOR = 0.05
 
     # ----------------------- MAIN FUNCTIONS ------------------------
 
@@ -24,6 +25,18 @@ class Player:
         self.bias = Player.global_bias
         self.round_total_payoff = 0
 
+        # Parameters used to compute the belief
+
+        # sum(1/(t_i + 1)), where i is the id of a node of your tag   
+        self.own_tag_inv_payoff = 0
+        self.other_tag_inv_payoff = 0
+
+        self.own_tag_cooperators = 0
+        self.other_tag_cooperators = 0       
+
+        self.own_tag_count = 0
+        self.other_tag_count = 0
+
         # Initialize network
         Player.network = arena.G
 
@@ -31,48 +44,49 @@ class Player:
         self.env = arena
         self.env.process(self.run())
 
-    def run(self, other_player):
-        while True:
-            self.play_with_neighour(other_player)
-            #! Gotta break this cycle with yields or events, somehow
+    # def run(self, other_player):
+    #     while True:
+    #         self.play_with_neighour(other_player)
+    #         #! Gotta break this cycle with yields or events, somehow
 
 
     # --------------------- PRISONER'S DILEMMA ----------------------
 
     def play_with_neighbour(self, other_player):
-        assert other_player.id in self.network[self.id]
-
         def choose_tactics():
             return self.choose_tactic(other_player), other_player.choose_tactic(self)
 
         def compute_payoffs():
             return self.compute_payoff(own_choice, other_choice), other_player.compute_payoff(other_choice, own_choice)
 
-        def update_biases():
-            self.update_bias(own_payoff, other_player.get_tag())
-            other_player.update_bias(other_payoff, self.get_tag())
-        
-        def increase_total_years_and_tag_counts(own_tag_inv_years, own_tag_neighs,
-                                                    other_tag_inv_years, other_tag_neighs,
-                                                        own_previous_payoff):
-            increase_in_years = self.round_total_payoff - own_previous_payoff
+        def update_counts_for_bias_update():
+            self.increase_total_years_and_tag_counts(other_player.get_tag(), own_payoff)
+            other_player.increase_total_years_and_tag_counts(self.get_tag(), other_payoff)
 
-            if self.get_tag() != other_player.get_tag():
-                other_tag_inv_years += 1 / increase_in_years
-                other_tag_neighs += 1
-            else:
-                own_tag_inv_years += 1 / increase_in_years
-                own_tag_neighs += 1
+            self.update_cooperator_count(other_choice, other_player.get_tag())
+            other_player.update_cooperator_count(own_choice, self.get_tag())
+
+        assert other_player.id in self.network[self.id]
 
         # Each node chooses its tactic
         own_choice, other_choice = choose_tactics()
 
         # Compute the payoff from playing with the other node
-        own_payoff, other_payoff = self.compute_payoff(own_choice, other_choice)
-        
-        update_biases()
+        own_payoff, other_payoff = compute_payoffs()
+
+        update_counts_for_bias_update()
+
+
+    def increase_total_years_and_tag_counts(self, other_node_tag, payoff):
+        if other_node_tag == self.get_tag():
+            self.own_tag_inv_payoff += 1 / (payoff + 1)
+            self.own_tag_count += 1
+        else:
+            self.other_tag_inv_payoff += 1 / (payoff + 1)
+            self.other_tag_count += 1
 
     def choose_tactic(self, other_player):
+        #! This is "wrong"..?
         cooperate_prob = self.bias[self.get_tag()][other_player.get_tag()]
         return np.random.choice(a=['C', 'D'], p=[cooperate_prob, 1-cooperate_prob])
 
@@ -90,11 +104,32 @@ class Player:
         self.round_total_payoff += inc
         
         return inc
+    
+    def update_cooperator_count(self, other_choice, other_tag):
+        if other_choice == 'C':
+            if other_tag == self.get_tag():
+                self.own_tag_cooperators += 1
+            else:
+                self.other_tag_cooperators += 1
 
-    def update_bias(self, payoff=None, other_player_tag=None):
-        def compute_delta(self):
-            return 
-        pass
+    def update_belief(self):
+        def delta():
+            return (1 / self.other_tag_count) * self.other_tag_inv_payoff - (1 / self.own_tag_count) * self.own_tag_inv_payoff
+        def sigmoid(x):
+            return 1 / (1 + math.exp(-x))
+
+        self.belief = sigmoid(self.belief * self.FORGETTING_FACTOR + delta())
+
+    def update_global_bias(self):
+        own_tag_defactors = self.own_tag_count - self.own_tag_cooperators
+        other_tag_defactors = self.other_tag_count - self.other_tag_cooperators
+
+        
+
+    def reset_counters(self):
+        self.own_tag_cooperators = self.other_tag_cooperators = 0
+        self.own_tag_inv_payoff = self.other_tag_inv_payoff = 0
+        self.own_tag_count = self.other_tag_count = 0
 
     # ---------------------- HELPER FUNCTIONS -----------------------
     
